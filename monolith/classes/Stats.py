0befbf7e5069440e4471288074e31ddc50699f90
Edoardo <edoardo.maione@gmail.com>
2019-10-30 12:52:07 +0100

    WIP: implementing stats

---
+++ monolith/classes/Stats.py
@@ -0,0 +1,14 @@
+from monolith.database import Story
+
+
+class Stats():
+    likes: int
+    dislikes: int
+
+    def __init__(self, user_id):
+        self.likes = 0
+        self.dislikes = 0
+
+
+
+
