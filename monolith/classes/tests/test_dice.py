fbd7d4a836cc791d96572f9a4b9ad99ec04c21e6
selman <selman.alp@hotmail.com.tr>
2019-11-04 09:37:41 +0100

    Update

---
+++ monolith/classes/tests/test_dice.py
@@ -0,0 +1,73 @@
+import glob
+import os
+import random as rnd
+import natsort
+
+# list of all available dice sets
+_DICE_SETS = [{'id': 1, 'name': 'basic', 'folder': './monolith/resources/basic_set'}]
+
+
+class Die:
+
+    def __init__(self, filename):
+        self.faces = []
+        self.pip = None
+        f = open(filename, "r")
+        lines = f.readlines()
+
+        for line in lines:
+            self.faces.append(line.replace("\n", ""))
+        self.throw_die()
+        f.close()
+
+    def throw_die(self):
+        if self.faces:  # pythonic for list is not empty
+            self.pip = rnd.choice(self.faces)
+            return self.pip
+        else:
+            raise IndexError("throw_die(): empty die error.")
+
+
+class DiceSet:
+
+    def __init__(self, set_name):
+        self.dice = []
+        self.pips = []
+
+        dice_folder = ""
+        for e in _DICE_SETS:
+            if e['name'] == set_name:
+                dice_folder = e['folder']
+
+        if dice_folder == "":
+            raise NonExistingSetError("Dice set not found")
+
+        folder = glob.glob(os.path.join(dice_folder, '*.txt'))
+        sorted(folder)
+
+        for filename in natsort.natsorted(folder, reverse=False):
+            die = Die(filename)
+            self.dice.append(die)
+
+    def throw_dice(self, dicenumber):
+        if(dicenumber<=len(self.dice)):
+            for i in range(dicenumber):
+                self.pips.append(self.dice[i].throw_die())
+        else:
+            raise WrongDiceNumberError("Not enough dice!")
+        return self.pips
+
+
+class NonExistingSetError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+class WrongDiceNumberError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
