19aac63305ddca39ecca0a6e98443f08450b3e2d
CB-92 <baragliachiara@yahoo.it>
2019-11-04 16:32:59 +0100

    Delete unused function

--- monolith/views/users.py
+++ monolith/views/users.py
@@ -25,20 +25,3 @@ def _users():
     users = db.session.query(User, Story).join(Story).order_by(desc(Story.id))
     users = reduce_list(users.all())
     return render_template("users.html", users=users)
-
-
-@users.route('/create_user', methods=['GET', 'POST'])
-def create_user():
-    form = UserForm()
-    if request.method == 'POST':
-
-        if form.validate_on_submit():
-            new_user = User()
-            form.populate_obj(new_user)
-            # pw should be hashed with some salt
-            new_user.set_password(form.password.data)
-            db.session.add(new_user)
-            db.session.commit()
-            return redirect('/users')
-
-    return render_template('create_user.html', form=form)
