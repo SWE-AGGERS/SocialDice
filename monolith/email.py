46a9bb9d6dad7a333f1df636c654ebaa0cef9c67
Khaos <danimorpg@gmail.com>
2019-11-05 12:28:16 +0100

    Addedget_followed_list func and some line for mail message

--- monolith/email.py
+++ monolith/email.py
@@ -1,7 +1,7 @@
 from celery import Celery
 from celery.schedule import crontab
 from monolith.constants import _EMAIL, _PASSWORD
-from monolith.database import db, Users
+from monolith.database import db, User, Story
 from monolith.views.follow import _get_followers_of
 # emails library
 import smtplib
@@ -41,23 +41,39 @@ def send_emails():
 
 def get_users():
 	"""Return all the users in the db"""
-	return Users.query.all()
+	return User.query.all()
 
 
 def maker_message(user):
 	"""Make personalized message for each user"""
 	text = "Hello "+user.name
-	followed_lists = _get_followers_of(user.id)
+	followed_lists = get_followed_list(user.id)
 	if followed_lists == []:
 		return text+",\n\nYou have no news for today, take a look and add new writers on Sweaggers' SocialDice!"
 	else:
-		text +=",\n\nhere you can find what's new on the wall of Sweaggers' SocialDice!\n\n"
+		text +=",\n\nhere you can find what's new on the wall of Sweaggers' SocialDice!\n"
 
 	for followed in followed_list:
 		# get all stories of a follower posted in the last 24h
 		# count them
-		# put a line in the text with "> <followed_user_name> poste <stories_number> new stories!"
+		stories_number = len(get_all_stories_by_writer(followed.))
+		
+		# put a line in the text with "<followed_user_name> posts <stories_number> new stories!"
+		if stories_number > 0:
+			text += "\n - "+user.firstname+" "+user.lastname+" posts "+str(stories_number)+"new stories."
 
-	text += "\nSee you on SocialDice,\nSeaggers Team"
+	text += "\nSee you on SocialDice,\nSweaggers Team"
 
 	return text
+
+
+def get_all_stories_by_writer(userid):
+	try:
+		res = Story.filter(and_(Story.author_id == userid, Story.date > [<TODAY - 24h]))
+		return res
+	except
+		return []
+
+
+def get_followed_list(userid):
+	return [f.followed for f in _get_followers_of(userid)]
