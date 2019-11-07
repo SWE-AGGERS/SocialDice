b6f235fb359b5a5bc99ea55a7505c57a108f5a77
Khaos <danimorpg@gmail.com>
2019-11-07 20:22:30 +0100

    fixing

--- monolith/views/check_stories.py
+++ monolith/views/check_stories.py
@@ -3,41 +3,33 @@ import nltk
 nltk.download('wordnet')
 
 
-
-
-
 def check_storyV2(story,diceSet):
     # Tests on parameters
     if not isinstance(story, str):
         raise WrongFormatStoryError("The story must be a string.\n")
+    if len(story)>1000:
+        raise TooLongStoryError("The story is too long. The length is > 1000 characters\n")
+    if not isinstance(diceSet, list):
+        raise WrongFormatDiceError("The dice set must be a list.\n")
     res = len(story.split())
-    if not isinstance(diceSet,ds.DiceSet):
-        raise WrongFormatDiceError("The dice set must be a DiceSet object.\n")
-    if res < len(diceSet.pips):
+    if res < len(diceSet):
         raise TooSmallStoryError(
             "The number of words of the story must greater or equal of the number of resulted faces.\n")
-    if len(diceSet.pips) != len(diceSet.dice):
-        raise SizeDiceSetFacesError("The number of resulting faces must be equal to the number of dice.\n")
-    for elem in diceSet.dice:
-        if not isinstance(elem,ds.Die):
-            raise WrongFormatSingleDiceError("Every dice of the dice set must be a die.\n")
-    for elem in diceSet.pips:
+    for elem in diceSet:
         if not isinstance(elem,str):
-            raise WrongFormatSingleFaceError("Every resulting face of the dice set must be a string.\n")
-
-
+            raise WrongFormatSingleDiceError("Every dice of the dice set must be a die.\n")
 
     found = True
     i = 0
-    while found and i<len(diceSet.pips):
+    while found and i < len(diceSet):
         arr = []
-        synset = nltk.corpus.wordnet.synsets(diceSet.pips[i])
+        synset = nltk.corpus.wordnet.synsets(diceSet[i])
         if len(synset) > 0:
             for j in range(0,len(synset[0].lemmas())):
                 arr.append(synset[0].lemmas()[j].name())
-        if(diceSet.pips[i] not in arr):
-            arr.append(diceSet.pips[i])
-        print("Synonyms of word "+diceSet.pips[i]+":")
+        if(diceSet[i] not in arr):
+            arr.append(diceSet[i])
+        print("Synonyms of word "+diceSet[i]+":")
         print(arr)
         foundWord = False
         k = 0
@@ -50,7 +42,10 @@ def check_storyV2(story,diceSet):
             found = False
         else:
             i = i+1
-    return found
+
+    if not found:
+        return InvalidStory
+
 
 
 class WrongFormatStoryError(Exception):
@@ -75,6 +70,12 @@ class WrongFormatDiceError(Exception):
         return repr(self.value)
 
 
+class TooLongStoryError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
 
 
 
@@ -94,6 +95,13 @@ class WrongFormatSingleDiceError(Exception):
     def __str__(self):
         return repr(self.value)
 
+
+class InvalidStory(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
 
 class WrongFormatSingleFaceError(Exception):
     def __init__(self, value):
